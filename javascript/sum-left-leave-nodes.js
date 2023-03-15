7da8b9d9fd846816abd2c43a556a2c72a297d059
Johan Alvarez <llstarscreamll@hotmail.com>
2023-03-14 23:06:51 -0500

    add some docs and another test case on sum left leave nodes

--- javascript/sum-left-leave-nodes.js
+++ javascript/sum-left-leave-nodes.js
@@ -1,3 +1,16 @@
+/**
+ * Given a Binary Tree, find the sum of all left leaves in it. For example, sum
+ * of all left leaves in below Binary Tree is 4 + 7 + 8 = 19.
+ *           1
+ *         /   \
+ *       2       5
+ *         \        \
+ *           3        6
+ *         /        /   \
+ *       4        7       7
+ *                      /
+ *                    8
+ */
 function sumLeftLeaveNodes(node) {
   let result = 0;
 
--- javascript/sum-left-leave-nodes.test.js
+++ javascript/sum-left-leave-nodes.test.js
@@ -61,6 +61,23 @@ describe.each([
   ],
   [
     () => {
+      let root = new Node(1);
+      root.left = new Node(2);
+      root.left.right = new Node(3);
+      root.left.right.left = new Node(4);
+      
+      root.right = new Node(5);
+      root.right.right = new Node(6);
+      root.right.right.right = new Node(7);
+      root.right.right.left = new Node(7);
+      root.right.right.right.left = new Node(8);
+
+      return root;
+    },
+    19,
+  ],
+  [
+    () => {
       let root = new Node(9);
       root.left = new Node(8);
       root.left.left = new Node(5);
