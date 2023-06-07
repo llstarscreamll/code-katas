4b9598a2d63e0b785735f6ceb2fee955e9d2f7c8
Johan Alvarez <llstarscreamll@hotmail.com>
2023-06-06 21:17:14 -0500

    update birthday cake candles challenge description

--- php/BirthdayCakeCandles.php
+++ php/BirthdayCakeCandles.php
@@ -5,7 +5,7 @@
 // be able to blow out the tallest of the candles. Count how many candles
 // are tallest. Example:
 // candles = [4,4,1,3]
-// The maximum height candles are  units high. There are  of them, so return .
+// The maximum height candles are 4 units high. There are 2 of them, so return 2.
 function birthdayCakeCandles($candles) {
     $r = [];
 
